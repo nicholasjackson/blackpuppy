d9a45e5f49ee6ff075d8e8ad2e80b30dd71bc762
Nic Jackson <jackson.nic@gmail.com>
2015-08-23 21:36:55 +0100

    updated maps, fixed layout on mobile

--- js/map.js
+++ js/map.js
@@ -3,11 +3,12 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
+    scrollwheel: false,
     center: {lat: 51.6095, lng: -0.8657701}
   });
 
   var ctaLayer = new google.maps.KmlLayer({
-    url: '/kml/blackpupp100.kml',
+    url: 'https://raw.githubusercontent.com/nicholasjackson/blackpuppy/master/kml/blackpuppy100.kml',
     map: map
   });
 }
